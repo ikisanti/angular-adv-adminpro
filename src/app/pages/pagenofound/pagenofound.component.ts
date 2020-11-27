aed4968a38611d69d4a437fb446ad3195553b486
ikisanti <santigonzalez@utp.edu.co>
2020-11-27 11:26:35 -0500

    first commit

---
+++ src/app/pages/pagenofound/pagenofound.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagenofound',
+  templateUrl: './pagenofound.component.html',
+  styleUrls: [ './pagenofound.component.css' ]
+})
+export class PagenofoundComponent  {
+
+  year = new Date().getFullYear();
+}
